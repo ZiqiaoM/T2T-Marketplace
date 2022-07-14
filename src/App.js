d8a683ac4cf3fa1a887e344603b1d56587371778
yranchan_local <yran.chan.tsukue@gmail.com>
2022-07-14 16:54:16 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
-// import Header from './Header';
-// import Hero from './Hero';
-// import Showpage from './Showpage';
-// import Footer from './Footer';
-// import Login from './Login';
-// import Signup from './Signup';
+import Header from './Header';
+import Hero from './Hero';
+import Showpage from './Showpage';
+import Footer from './Footer';
+import Login from './Login';
+import Signup from './Signup';
 import Post from './Post';
-
+import Layer1 from './Layer1';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <Hero />
-      <Showpage />
-      <Footer /> */}
-      {/* <Login /> */}
-      {/* <Signup /> */}
-      <Post />
+
+      {/* <Showpage /> */}
+      <Layer1/>
+      <Footer />
+
+
+//登录注册功能
+      {/* <Login />
+      <Signup /> */}
+
+//商品发布功能
+
+      {/* <Post />  */}
+
+
     </div>
   );
 }
