4c9833d6af8dfd1e3173501b560ac370616069cc
ZiqiaoM <ziqiao.ma@unc.edu>
2023-01-30 22:30:55 -0500

    0130

---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import styles from "./index.module.css";
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import Layout from "../layout";
+import Header from "../header"
+
+import { api } from "../utils/api";
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <main>
+    {/* <PersonalInfo /> */}
+    <Header/>
+
+    </main>
+  );
+};
+
+export default Home;
+
+// const AuthShowcase: React.FC = () => {
+//   const { data: sessionData } = useSession();
+
+//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+//     undefined, // no input
+//     { enabled: sessionData?.user !== undefined },
+//   );
+
+//   return (
+//     <div className={styles.authContainer}>
+//       <p className={styles.showcaseText}>
+//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+//         {secretMessage && <span> - {secretMessage}</span>}
+//       </p>
+//       <button
+//         className={styles.loginButton}
+//         onClick={sessionData ? () => void signOut() : () => void signIn()}
+//       >
+//         {sessionData ? "Sign out" : "Sign in"}
+//       </button>
+//     </div>
+//   );
+// };
