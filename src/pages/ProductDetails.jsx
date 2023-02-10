23c5e437ba696a0fbd413f2a42e8c8200adbcba1
DidiQut <iamzhidi@gmail.com>
2023-02-09 20:16:58 -0500

    add productdetail

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,18 +1,242 @@
-// import React, {useState, useEffect} from 'react';
-// import products from '../sample-data/products';
-// import { useParams } from 'react-router-dom';
-import Helmet from '../components/Helmet/Helmet';
-import {Container, Row, Col} from "reactstrap";
-// import CommonSection from '../components/UI/common-section/CommonSection'
-// import productImg from '../images/product_01_image_01.jpg'
-// import './ProductDetails.css'
+// import React, { useState, useEffect } from "react";
+// import products from "../sample-data/products";
+// import Helmet from "../components/Helmet/Helmet";
+// import { Container, Row, Col } from "reactstrap";
 
+// const ProductDetails = () => {
+//   return (
+//   <div>
+//     <h1>???????????</h1>
+//   </div>
+//   )
+// };
+// export default ProductDetails;
 
-const ProductDetails = () => {
-  return (
-  <div>
-    <h1>???????????</h1>
-  </div>
-  )
+// tailwind.config.js
+// module.exports = {
+//   // ...
+//   plugins: [
+//     // ...
+//     require('@tailwindcss/typography'),
+//   ],
+// }
+// ```
+
+import { useState } from "react";
+import { Disclosure, RadioGroup, Tab } from "@headlessui/react";
+import { StarIcon } from "@heroicons/react/20/solid";
+import {
+  HeartIcon,
+  MinusSmIcon,
+  PlusSmIcon,
+} from "@heroicons/react/24/outline";
+
+const product = {
+  name: "Basic Tee 6-Pack",
+  price: "$192",
+  href: "#",
+  // breadcrumbs: [
+  //   { id: 1, name: "Men", href: "#" },
+  //   { id: 2, name: "Clothing", href: "#" },
+  // ],
+  images: [
+    {
+      id: 1,
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
+      alt: "Two each of gray, white, and black shirts laying flat.",
+      primary: true,
+    },
+    {
+      id: 2,
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+      alt: "Model wearing plain black basic tee.",
+      primary: false,
+    },
+    {
+      id: 3,
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+      alt: "Model wearing plain gray basic tee.",
+      primary: false,
+    },
+    {
+      id: 4,
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+      alt: "Model wearing plain white basic tee.",
+      primary: false,
+    },
+  ],
+  // colors: [
+  //   { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
+  //   { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+  //   { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+  // ],
+  description:
+    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+  highlights: [
+    "Hand cut and sewn locally",
+    "Dyed with our proprietary colors",
+    "Pre-washed & pre-shrunk",
+    "Ultra-soft 100% cotton",
+  ],
+  details:
+    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
 };
-export default ProductDetails;
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="pt-6">
+        {/* Navigation */}
+        {/* <nav aria-label="Breadcrumb">
+          <ol
+            role="list"
+            className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
+          >
+            {product.breadcrumbs.map((breadcrumb) => (
+              <li key={breadcrumb.id}>
+                <div className="flex items-center">
+                  <a
+                    href={breadcrumb.href}
+                    className="mr-2 text-sm font-medium text-gray-900"
+                  >
+                    {breadcrumb.name}
+                  </a>
+                  <svg
+                    width={16}
+                    height={20}
+                    viewBox="0 0 16 20"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="h-5 w-4 text-gray-300"
+                  >
+                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                  </svg>
+                </div>
+              </li>
+            ))}
+            <li className="text-sm">
+              <a
+                href={product.href}
+                aria-current="page"
+                className="font-medium text-gray-500 hover:text-gray-600"
+              >
+                {product.name}
+              </a>
+            </li>
+          </ol>
+        </nav> */}
+
+        {/* Image gallery */}
+        <Tab.Group as="div" className="flex flex-col-reverse">
+          {/* Image selector */}
+          <div className="hidden mt-6 w-full max-w-2xl mx-auto sm:block lg:max-w-none">
+            <Tab.List className="grid grid-cols-4 gap-6">
+              {product.images.map((image) => (
+                <Tab
+                  key={image.id}
+                  className="relative h-24 bg-white rounded-md flex items-center justify-center text-sm font-medium uppercase text-gray-900 cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-offset-4 focus:ring-opacity-50"
+                >
+                  {({ selected }) => (
+                    <>
+                      <span className="sr-only">{image.name}</span>
+                      <span className="absolute inset-0 rounded-md overflow-hidden">
+                        <img
+                          src={image.src}
+                          alt=""
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </span>
+                      <span
+                        className={classNames(
+                          selected ? "ring-indigo-500" : "ring-transparent",
+                          "absolute inset-0 rounded-md ring-2 ring-offset-2 pointer-events-none"
+                        )}
+                        aria-hidden="true"
+                      />
+                    </>
+                  )}
+                </Tab>
+              ))}
+            </Tab.List>
+          </div>
+
+          <Tab.Panels className="w-full aspect-w-1 aspect-h-1">
+            {product.images.map((image) => (
+              <Tab.Panel key={image.id}>
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full h-full object-center object-cover sm:rounded-lg"
+                />
+              </Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+
+        {/* Product info */}
+        <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+              {product.name}
+            </h1>
+          </div>
+
+          {/* Options */}
+          <div className="mt-4 lg:row-span-3 lg:mt-0">
+            <h2 className="sr-only">Product information</h2>
+            <p className="text-3xl tracking-tight text-gray-900">
+              {product.price}
+            </p>
+
+            <form className="mt-10">
+              <button
+                type="submit"
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Add to bag
+              </button>
+            </form>
+          </div>
+
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+            {/* Description and details */}
+            <div>
+              <h3 className="sr-only">Description</h3>
+
+              <div className="space-y-6">
+                <p className="text-base text-gray-900">{product.description}</p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+              <div className="mt-4">
+                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                  {product.highlights.map((highlight) => (
+                    <li key={highlight} className="text-gray-400">
+                      <span className="text-gray-600">{highlight}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-sm text-gray-600">{product.details}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/pages/ProductDetails.jsx"],
+  theme: {
+    // ...
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    // ...
+  ],
+};
