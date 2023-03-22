06351415b64b178d4fffbb591ab821632a5cc00b
DidiQut <iamzhidi@gmail.com>
2023-03-22 15:56:03 -0400

    add some change

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -12,3 +12,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 export default prisma;
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = global.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+// export default prisma;
