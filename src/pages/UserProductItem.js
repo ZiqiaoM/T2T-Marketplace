20e36abbc5515c8703e04177cec71402efface9d
DidiQut <iamzhidi@gmail.com>
2023-04-19 23:15:58 -0400

    change font and userproductitem

--- src/pages/UserProductItem.js
+++ src/pages/UserProductItem.js
@@ -1,88 +1,39 @@
-import { ListGroupItem } from "reactstrap";
-
-import CloseIcon from "@mui/icons-material/Close";
 import Link from "next/link";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../store/wish-list/cartSlice";
 
-// export async function getStaticPaths(req) {
-//   const user = req.user;
-//   const paths = await prisma.product.findMany({
-//     where: {
-//       seller_id: user.id,
-//     },
-//     select: {
-//       id: true,
-//     },
-//   });
-
-//   paths.forEach((p) => {
-//     p.id = p.id.toString();
-//   });
-//   console.log(paths);
-//   return {
-//     paths: paths.map((id) => ({ params: id })),
-//     fallback: false,
-//   };
-// }
-
-// export async function getStaticProps({ params, req }) {
-//   const user = req.user;
-//   const product = await prisma.product.findUnique({
-//     where: {
-//       id: parseInt(params.id),
-//       seller_id: user.id,
-//     },
-//     select: {
-//       id: true,
-//       post_title: true,
-//       images: {
-//         select: {
-//           src: true,
-//         },
-//       },
-//     },
-//   });
-
-//   return {
-//     props: {
-//       product,
-//     },
-//   };
-// }
-
 const UserProductItem = (props) => {
-  const  product  = props.product;
+  const product = props.product;
   if (!product) {
     return (
       <div>
         <h6 className="text-center mt-5">
-          You have not uploaded any products yet.
+          You haven't uploaded any products yet.
         </h6>
       </div>
     );
   }
 
-  const { id, price,post_title, images } = product;
+  const { id, price, post_title, images } = product;
+  const dispatch = useDispatch();
 
-  console.log(product);
-
-  // const dispatch = useDispatch();
-
-  // //delete item
-  // const deleteItem = () => {
-  //   dispatch(cartActions.deleteItem(id));
-  // };
+  //delete item
+  const deleteItem = () => {
+    dispatch(cartActions.deleteItem(id));
+  };
 
   return (
-    <ListGroupItem className="border-0 cart_item">
-      <div className="ml-4 flex-1 flex flex-col sm:ml-6">
-        <div className="cart_item-info d-flex gap-2">
+    <ul>
+      {product.map((item) => {
+        const { id, post_title, price, images } = item;
+        const image = images[0].src;
+
+        return (
           <li key={id} className="flex py-6">
-            <Link passHref legacyBehavior href={`/productDetails/${id}`}>
-              <div className="flex-shrink-0">
+            <Link passHref href={`/productDetails/${id}`}>
+              <div className="flex-shrink-0 bg-white-200 rounded-lg overflow-hidden ">
                 <img
-                  src={images}
+                  src={image}
                   className="w-24 h-24 rounded-md object-center object-cover sm:w-32 sm:h-32"
                 />
               </div>
@@ -90,29 +41,25 @@ const UserProductItem = (props) => {
             <div className="ml-4 flex-1 flex flex-col sm:ml-6">
               <div className="flex justify-between">
                 <h4 className="text-sm">
-                  <a
-                    passHref
-                    legacyBehavior
-                    href={`/productDetails/${id}`}
-                    className="font-medium text-gray-700 hover:text-gray-800"
-                  >
-                    {post_title}
-                  </a>
+                  <Link passHref href={`/productDetails/${id}`}>
+                    <div className="font-medium text-gray-700 hover:text-gray-800">
+                      {post_title}
+                    </div>
+                  </Link>
                 </h4>
               </div>
               <p className="mt-1 text-sm text-gray-500">Price: ${price}</p>
               <span
                 className="delete_btn text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                // onClick={}
+                onClick={() => deleteItem(id)}
               >
-                <CloseIcon />
                 remove
               </span>
             </div>
           </li>
-        </div>
-      </div>
-    </ListGroupItem>
+        );
+      })}
+    </ul>
   );
 };
 
